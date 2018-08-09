fcd232031e9863c665a10f09f6811af4feff9585
Reilly <reilly.wood@icloud.com>
2018-08-08 21:51:16 -0700

    Move Azure config and HTML parsing out of main function body

--- RezoningScraper.Functions/RezoningScraper.cs
+++ RezoningScraper.Functions/RezoningScraper.cs
@@ -25,22 +25,110 @@ namespace AbundantHousingVancouver
         private static string RezoningPageUri;
         private static bool IsRunningInTestMode;
 
+        private static CloudStorageAccount StorageAccount;
+        private static CloudTable RezoningsTable;
+
         [FunctionName("RezoningScraper")]
         public async static Task Run([TimerTrigger("%TimerSchedule%", RunOnStartup = true)]TimerInfo myTimer, TraceWriter log, ExecutionContext context)
         {
             setUpLogging(log);
             loadConfiguration(context);
-
-            var storageAccount = CloudStorageAccount.Parse(StorageConnectionString);
-            var rezoningsTable = await getRezoningsTableAndCreateIfDoesntExist(storageAccount);
-            var rezonings = await GetRezoningsFromDb(rezoningsTable);
+            await loadAzureStorageObjects();
 
             var webpageHtml = new HtmlWeb().Load(RezoningPageUri);
-            saveHtmlToBlobStorage(storageAccount, webpageHtml, log);
+            saveHtmlToBlobStorage(StorageAccount, webpageHtml);
 
-            // The big ugly HTML parser. This is closely tied to the CoV formatting which could change at any time
-            // so it's probably not worth spending a lot of time cleaning it up
-            var desc = webpageHtml.DocumentNode.Descendants("li");
+            var existingRezonings = await GetRezoningsFromDb(RezoningsTable);
+            parseHtmlAndProcessUpdates(webpageHtml, existingRezonings);
+        }
+
+        private static void setUpLogging(TraceWriter _log)
+        {
+            Log = _log;
+        }
+
+        private static void loadConfiguration(ExecutionContext context)
+        {
+            var config = new ConfigurationBuilder()
+            .SetBasePath(context.FunctionAppDirectory)
+            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+            IsRunningInTestMode = Boolean.Parse(config["TestMode"] ?? "false");
+            StorageConnectionString = config["StorageConnectionString"];
+            RezoningPageUri = config["RezoningPageUri"];
+            SlackMessageUri = config["SlackMessageUri"];
+
+            if (IsRunningInTestMode)
+            {
+                Log.Info("Running in test mode");
+            }
+        }
+
+        private async static Task loadAzureStorageObjects()
+        {
+            StorageAccount = CloudStorageAccount.Parse(StorageConnectionString);
+            RezoningsTable = await getRezoningsTableAndCreateIfDoesntExist(StorageAccount);
+        }
+
+        private async static Task<CloudTable> getRezoningsTableAndCreateIfDoesntExist(CloudStorageAccount storageAccount)
+        {
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("rezonings");
+            await table.CreateIfNotExistsAsync();
+            return table;
+        }
+
+        private static void saveHtmlToBlobStorage(CloudStorageAccount storageAccount, HtmlDocument doc)
+        {
+            var saveFileName = $"VancouverRezoningWebpage-{DateTime.UtcNow.ToString("yyyyMMddTHHmmss")}.html";
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("rezoning-scrapes");
+            SaveTextFileToAzure(container, doc.DocumentNode.OuterHtml, saveFileName);
+            Log.Info($"Saved HTML to {saveFileName}");
+        }
+        private async static void SaveTextFileToAzure(CloudBlobContainer container, string fileContents, string fileName)
+        {
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+            await blockBlob.UploadTextAsync(fileContents);
+        }
+
+        private static void SendMessageToSlack(string message)
+        {
+            Log.Info($"Sending message to Slack: {message}");
+            if (IsRunningInTestMode)
+                return;
+            var client = new HttpClient();
+            var content = new StringContent($"{{\"text\":\"{message}\"}}", Encoding.UTF8, "application/json");
+            var response = client.PostAsync(SlackMessageUri, content).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Failed to send message to slack: {response}");
+            }
+        }
+        private async static Task<List<Rezoning>> GetRezoningsFromDb(CloudTable table)
+        {
+            var ret = new List<Rezoning>();
+            var query = new TableQuery<Rezoning>();
+            TableContinuationToken token = null;
+
+            do
+            {
+
+                TableQuerySegment<Rezoning> seg = await table.ExecuteQuerySegmentedAsync<Rezoning>(query, token);
+                token = seg.ContinuationToken;
+                ret.AddRange(seg);
+
+            } while (token != null);
+
+            return ret;
+        }
+
+        // The big ugly HTML parser. This should really be rewritten, but it's closely tied to the CoV formatting 
+        // which could change at any time, so it's probably not worth spending time cleaning it up
+        private static void parseHtmlAndProcessUpdates(HtmlDocument html, List<Rezoning> existingRezonings)
+        {
+            var desc = html.DocumentNode.Descendants("li");
             var links = desc.Where(d => d.ChildNodes.Any() && d.ChildNodes.First().Name.Equals("a", StringComparison.OrdinalIgnoreCase));
             foreach (var htmlNode in links)
             {
@@ -56,10 +144,10 @@ namespace AbundantHousingVancouver
                 var parsed = ParsePostLinkString(afterLinkText);
                 var scrapedRezoning = new Rezoning(name) { Name = name, Status = parsed.status, Info = parsed.info };
 
-                var rezoningsWithSameName = rezonings.Where(r => r.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+                var rezoningsWithSameName = existingRezonings.Where(r => r.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
                 if (rezoningsWithSameName.Any())
                 {
-                    log.Info($"Existing entry for {name} found.");
+                    Log.Info($"Existing entry for {name} found.");
                     var oldVersion = rezoningsWithSameName.Single();
                     var changeDetails = new List<string>();
                     if (!scrapedRezoning.Status.Equals(oldVersion.Status, StringComparison.OrdinalIgnoreCase))
@@ -90,99 +178,23 @@ namespace AbundantHousingVancouver
                     {
                         oldVersion.Status = scrapedRezoning.Status;
                         oldVersion.Info = scrapedRezoning.Info;
-                        UpdateRezoningInDb(rezoningsTable, oldVersion);
+                        UpdateRezoningInDb(RezoningsTable, oldVersion);
                         var message = $"Rezoning application updated: *<{fullUri.ToString()}|{scrapedRezoning.Name}>*\n";
                         message += String.Join("\n", changeDetails);
-                        SendMessageToSlack(message, log);
+                        SendMessageToSlack(message);
                     }
                 }
                 else
                 {
-                    log.Info($"Writing new rezoning with name='{scrapedRezoning.Name}', Status='{scrapedRezoning.Status}', Info='{scrapedRezoning.Info}' ");
-                    InsertRezoningToDb(rezoningsTable, scrapedRezoning);
+                    Log.Info($"Writing new rezoning with name='{scrapedRezoning.Name}', Status='{scrapedRezoning.Status}', Info='{scrapedRezoning.Info}' ");
+                    InsertRezoningToDb(RezoningsTable, scrapedRezoning);
                     var message = $"New rezoning application: *<{fullUri.ToString()}|{scrapedRezoning.Name}>*\nStatus: {scrapedRezoning.Status}";
                     if (!String.IsNullOrEmpty(scrapedRezoning.Info))
                         message += $"\n{scrapedRezoning.Info}";
-                    SendMessageToSlack(message, log);
-                    log.Info("Wrote to DB");
+                    SendMessageToSlack(message);
+                    Log.Info("Wrote to DB");
                 }
             }
-        }
-
-        private static void setUpLogging(TraceWriter _log)
-        {
-            Log = _log;
-        }
-
-        private static void loadConfiguration(ExecutionContext context)
-        {
-            var config = new ConfigurationBuilder()
-            .SetBasePath(context.FunctionAppDirectory)
-            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-            .AddEnvironmentVariables()
-            .Build();
-            IsRunningInTestMode = Boolean.Parse(config["TestMode"] ?? "false");
-            StorageConnectionString = config["StorageConnectionString"];
-            RezoningPageUri = config["RezoningPageUri"];
-            SlackMessageUri = config["SlackMessageUri"];
-
-            if (IsRunningInTestMode)
-            {
-                Log.Info("Running in test mode");
-            }
-        }
-
-        private async static Task<CloudTable> getRezoningsTableAndCreateIfDoesntExist(CloudStorageAccount storageAccount)
-        {
-            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-            CloudTable table = tableClient.GetTableReference("rezonings");
-            await table.CreateIfNotExistsAsync();
-            return table;
-        }
-
-        private static void saveHtmlToBlobStorage(CloudStorageAccount storageAccount, HtmlDocument doc, TraceWriter log)
-        {
-            var saveFileName = $"VancouverRezoningWebpage-{DateTime.UtcNow.ToString("yyyyMMddTHHmmss")}.html";
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-            CloudBlobContainer container = blobClient.GetContainerReference("rezoning-scrapes");
-            SaveTextFileToAzure(container, doc.DocumentNode.OuterHtml, saveFileName);
-            log.Info($"Saved HTML to {saveFileName}");
-        }
-        private async static void SaveTextFileToAzure(CloudBlobContainer container, string fileContents, string fileName)
-        {
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
-            await blockBlob.UploadTextAsync(fileContents);
-        }
-
-        private static void SendMessageToSlack(string message, TraceWriter log)
-        {
-            log.Info($"Sending message to Slack: {message}");
-            if (IsRunningInTestMode)
-                return;
-            var client = new HttpClient();
-            var content = new StringContent($"{{\"text\":\"{message}\"}}", Encoding.UTF8, "application/json");
-            var response = client.PostAsync(SlackMessageUri, content).Result;
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception($"Failed to send message to slack: {response}");
-            }
-        }
-        private async static Task<List<Rezoning>> GetRezoningsFromDb(CloudTable table)
-        {
-            var ret = new List<Rezoning>();
-            var query = new TableQuery<Rezoning>();
-            TableContinuationToken token = null;
-
-            do
-            {
-
-                TableQuerySegment<Rezoning> seg = await table.ExecuteQuerySegmentedAsync<Rezoning>(query, token);
-                token = seg.ContinuationToken;
-                ret.AddRange(seg);
-
-            } while (token != null);
-
-            return ret;
         }
 
         private async static void InsertRezoningToDb(CloudTable table, Rezoning r)
