c8ec4f5bc3e52729df49c62867c280dccb670116
Reilly Wood <reilly.wood@icloud.com>
2021-11-29 12:55:01 -0800

    Add script to publish trimmed exe for all OSs

---
+++ publish-all.sh
@@ -0,0 +1,3 @@
+dotnet publish --configuration Release -p:PublishProfile=win-x64
+dotnet publish --configuration Release -p:PublishProfile=linux-x64
+dotnet publish --configuration Release -p:PublishProfile=osx-x64
