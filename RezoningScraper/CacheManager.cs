b3b55495b7653bba5bc538cd21611d02eb2fd8ab
Reilly Wood <reilly.wood@icloud.com>
2022-02-27 21:21:57 -0800

    clean up cache manager

--- RezoningScraper/CacheManager.cs
+++ RezoningScraper/CacheManager.cs
@@ -5,71 +5,73 @@ using System.Text;
 using static Spectre.Console.AnsiConsole;
 using Dapper;
 
-namespace RezoningScraper
+namespace RezoningScraper;
+
+public class CacheManager<TResult> : IAsyncCacheProvider<TResult>
 {
-    public class CacheManager<TResult> : IAsyncCacheProvider<TResult>
+    public async Task PutAsync(string key, TResult value, Ttl ttl, CancellationToken ct, bool continueOnCapturedContext)
     {
-        public async Task PutAsync(string key, TResult value, Ttl ttl, CancellationToken ct, bool continueOnCapturedContext)
+        using var dbConnection = DbHelper.CreateOrOpenFileDb("RezoningScraper.db");
+        using var sha = SHA256.Create();
+        var hashbytes = sha.ComputeHash(Encoding.UTF8.GetBytes(key));
+        var hashstring = new StringBuilder();
+        foreach (var b in hashbytes)
         {
-            using var dbConnection = DbHelper.CreateOrOpenFileDb("RezoningScraper.db");
-            using var sha = SHA256.Create();
-            var hashbytes = sha.ComputeHash(Encoding.UTF8.GetBytes(key));
-            var hashstring = new StringBuilder();
-            foreach (var b in hashbytes)
-            {
-                hashstring.Append(b.ToString("X2"));
-            }
-
-            using var ms = new MemoryStream();
-            await JsonSerializer.SerializeAsync(ms, value, cancellationToken: ct);
-
-            
-            using var tran = dbConnection.BeginTransaction();
-            dbConnection.Execute(
-                "REPLACE INTO Cache(Key, Expiration, Value) VALUES(@Key, @Expiration,@Value)",
-                new {
-                    Key = hashstring.ToString(),
-                    Expiration = (DateTimeOffset.UtcNow + ttl.Timespan).ToUnixTimeMilliseconds(),
-                    Value = Encoding.UTF8.GetString(ms.ToArray())
-                });
-
-            tran.Commit();
-            WriteLine($"Completed put cache for {key}");
+            hashstring.Append(b.ToString("X2"));
         }
 
-        public async Task<(bool, TResult)> TryGetAsync(string key, CancellationToken ct, bool continueOnCapturedContext)
-        {
-            using var dbConnection = DbHelper.CreateOrOpenFileDb("RezoningScraper.db");
-            using var sha = SHA256.Create();
-            var hashbytes = sha.ComputeHash(Encoding.UTF8.GetBytes(key));
-            var hashstring = new StringBuilder();
-            foreach (var b in hashbytes)
-            {
-                hashstring.Append(b.ToString("X2"));
-            }
+        using var ms = new MemoryStream();
+        await JsonSerializer.SerializeAsync(ms, value, cancellationToken: ct);
 
-            var cachedValue = await dbConnection.ExecuteScalarAsync<string>("SELECT Value FROM Cache WHERE Key = @Key AND Expiration > @Expiration",
-                new
-                {
-                    Key=hashstring.ToString(),
-                    Expiration=DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
-                });
-            try
+
+        using var tran = dbConnection.BeginTransaction();
+        dbConnection.Execute(
+            "REPLACE INTO Cache(Key, Expiration, Value) VALUES(@Key, @Expiration,@Value)",
+            new
             {
-                if (string.IsNullOrEmpty(cachedValue))
-                {
-                    WriteLine($"No value in cache for {key}");
-                    return (false, default(TResult));
-                }
-                var model = JsonSerializer.Deserialize<TResult>(cachedValue);
-                    WriteLine($"Found value in cache for {key}");
-                    return (true, model);
-            }
-            catch
+                Key = hashstring.ToString(),
+                Expiration = (DateTimeOffset.UtcNow + ttl.Timespan).ToUnixTimeMilliseconds(),
+                Value = Encoding.UTF8.GetString(ms.ToArray())
+            });
+
+        tran.Commit();
+        WriteLine($"Completed put cache for {key}");
+    }
+
+    public async Task<(bool, TResult)> TryGetAsync(string key, CancellationToken ct, bool continueOnCapturedContext)
+    {
+        using var dbConnection = DbHelper.CreateOrOpenFileDb("RezoningScraper.db");
+        using var sha = SHA256.Create();
+        var hashbytes = sha.ComputeHash(Encoding.UTF8.GetBytes(key));
+        var hashstring = new StringBuilder();
+        foreach (var b in hashbytes)
+        {
+            hashstring.Append(b.ToString("X2"));
+        }
+
+        var cachedValue = await dbConnection.ExecuteScalarAsync<string>("SELECT Value FROM Cache WHERE Key = @Key AND Expiration > @Expiration",
+            new
             {
-                WriteLine($"Error on get cache for {key}");
+                Key = hashstring.ToString(),
+                Expiration = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
+            });
+#pragma warning disable CS8619
+        try
+        {
+            if (string.IsNullOrEmpty(cachedValue))
+            {
+                WriteLine($"No value in cache for {key}");
                 return (false, default(TResult));
             }
+            var model = JsonSerializer.Deserialize<TResult>(cachedValue);
+            WriteLine($"Found value in cache for {key}");
+            return (true, model);
         }
+        catch
+        {
+            WriteLine($"Error on get cache for {key}");
+            return (false, default(TResult));
+        }
+#pragma warning restore CS8619
     }
 }
