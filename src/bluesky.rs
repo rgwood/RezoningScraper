459182e644b0c3997aadfa0ef956e00d40fd48e9
Reilly Wood <reilly.wood@icloud.com>
2024-12-19 19:48:18 -0800

    Exclude generic images

--- src/bluesky.rs
+++ src/bluesky.rs
@@ -17,31 +17,32 @@ pub async fn post_to_bluesky(
     // TODO: persist the token?
     _ = agent.login(username, password).await?;
 
-    // TODO: upload image
-
     let mut embed = None;
 
     if let Some(img_url) = &project.attributes.image_url {
-        let img = reqwest::get(img_url).await?.bytes().await?;
-        eprintln!("Downloaded image");
+        // sometimes they post generic images that we don't want to repost
+        if !img_url.to_lowercase().contains("generic") {
+            let img = reqwest::get(img_url).await?.bytes().await?;
+            eprintln!("Downloaded image: {}", img_url);
 
-        let output = agent.api.com.atproto.repo.upload_blob(img.to_vec()).await?;
-        eprintln!("Uploaded image");
+            let output = agent.api.com.atproto.repo.upload_blob(img.to_vec()).await?;
+            eprintln!("Uploaded image");
 
-        let image = atrium_api::app::bsky::embed::images::ImageData {
-            alt: "Project image".to_string(),
-            aspect_ratio: None,
-            image: output.data.blob,
+            let image = atrium_api::app::bsky::embed::images::ImageData {
+                alt: "Project image".to_string(),
+                aspect_ratio: None,
+                image: output.data.blob,
+            }
+            .into();
+
+            let images = vec![image];
+
+            embed = Some(Union::Refs(
+                atrium_api::app::bsky::feed::post::RecordEmbedRefs::AppBskyEmbedImagesMain(
+                    Box::new(atrium_api::app::bsky::embed::images::MainData { images }.into()),
+                ),
+            ));
         }
-        .into();
-
-        let images = vec![image];
-
-        embed = Some(Union::Refs(
-            atrium_api::app::bsky::feed::post::RecordEmbedRefs::AppBskyEmbedImagesMain(Box::new(
-                atrium_api::app::bsky::embed::images::MainData { images }.into(),
-            )),
-        ));
     }
 
     let tweet_with_link = format!("{} {}", tweet_text, project.links.self_link);
